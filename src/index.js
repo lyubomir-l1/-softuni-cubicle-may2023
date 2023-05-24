ba2da41ec4157e8655698395eeb77a842de2c8e2
Lyubomir Lyubenov <lyubomir2017@gmail.com>
2023-05-24 20:18:34 +0300

    add express to project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+const express = require('express');
+
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from express')
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`))
