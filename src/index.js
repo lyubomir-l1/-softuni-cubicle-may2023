ec70a2f8218cca1e8c9d3da5734bab17f677c7b5
Lyubomir Lyubenov <lyubomir2017@gmail.com>
2023-05-27 19:31:06 +0300

    add cubeController

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
 const homeController = require('./controllers/homeController');
+const cubeController = require('./controllers/cubeController')
 
 const app = express();
 const PORT = 5000;
@@ -12,6 +13,7 @@ expressConfig(app);
 handlebarsConfig(app);
 
 app.use(homeController);
+app.use('/cubes', cubeController)
 //app.get('/', homeController.getHome);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`))
