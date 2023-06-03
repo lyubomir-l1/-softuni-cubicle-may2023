555867a263b3c1432684595601cb2f9ccbf030f2
Lyubomir Lyubenov <lyubomir2017@gmail.com>
2023-06-03 20:03:47 +0300

    install mongoose and add base Cube model

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 
-const path = require('path');
+// const path = require('path');
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
+const dbConnect = require('./config/dbConfig');
 const routes = require('./routes');
 
 const app = express();
 const PORT = 5000;
 
+
 expressConfig(app);
 handlebarsConfig(app);
+
+dbConnect()
+.then(() => console.log('DB connected successfully'))
+.catch(err => {
+    console.log('DB error: ', err);
+});
 
  app.use(routes);
 
