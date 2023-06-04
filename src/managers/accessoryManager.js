d7a92d435461db8ec5fa0c35dcce297d94234e5b
Lyubomir Lyubenov <lyubomir2017@gmail.com>
2023-06-04 19:24:12 +0300

    show remaining accessories only

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -2,3 +2,5 @@ const Accessory = require('../models/Accessory');
 
 exports.getAll = () => Accessory.find();
 exports.create = (accessoryData) => Accessory.create(accessoryData);
+exports.getOthers = (accessoriesIds) => Accessory.find({ _id: { $nin: accessoriesIds } })
+// exports.getOthers = (accessoriesIds) => Accessory.find().nin()
