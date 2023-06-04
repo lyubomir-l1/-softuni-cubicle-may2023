34449761817c66ebc796c4107baa636b81ca4d6c
Lyubomir Lyubenov <lyubomir2017@gmail.com>
2023-06-04 19:01:25 +0300

    add relation from cube to accessory

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -25,3 +25,11 @@ await cube.save();
 
 return cube;
 } 
+
+exports.attachAccessory = async (cubeId, accessoryId) => {
+// return Cube.findByIdAndUpdate(cubeId, {$push: { accessories: accessoryId } });
+const cube = await Cube.findById(cubeId);
+cube.accessories.push(accessoryId);
+return cube.save();
+
+};
